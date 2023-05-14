6879ed87198510779620c2c391949eb5cb22f0b3
amanzhol <rysbek_dauren@mail.ru>
2023-05-15 01:19:12 +0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+class TestingClass{
+    private int val;
+    public TestingClass(int v) {
+        this.val = v;
+    }
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(val);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashTable hashTable = new MyHashTable<MyTestingClass, String>();
+        Random rand = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int val = (int )Math.random();
+            hashTable.put(new TestingClass(val), "Student " + val);
+        }
+
     }
 }
