bb756dd3b1cabd1b6935bb01f169a09cdb99a8f3
amanzhol <rysbek_dauren@mail.ru>
2023-05-15 01:09:51 +0600

    MyTestingClass

---
+++ src/MyTestingClass.java
@@ -0,0 +1,29 @@
+ /*
+   MyTestingClass - constructor
+ */
+public class MyTestingClass<K, V> {
+    private K key;
+    private V value;
+    public MyTestingClass(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + key + " " + value + "}";
+    }
+
+}
